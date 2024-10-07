766fb46cf33922aebf9f3ae2d10b964ac6dd4f16
Kyle Bashour <kylebshr@me.com>
2024-10-07 11:22:47 +0900

    Remove docc dependency

--- Package.swift
+++ Package.swift
@@ -10,9 +10,6 @@ let package = Package(
     products: [
         .library(name: "Pinnable", targets: ["Pinnable"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    ],
     targets: [
         .target(name: "Pinnable"),
     ]
